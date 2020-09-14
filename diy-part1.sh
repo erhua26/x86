6ab2481438566b14c0bfdf0d13f43c1ad21d61cf
erhua26 <35597700+erhua26@users.noreply.github.com>
2020-09-14 19:42:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-src-git https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-src-git https://github.com/kenzok8/small.git package/openwrt-small
-src-git https://github.com/fw876/helloworld package/openwrt-packages
-src-git https://github.com/jefferymvp/luci-app-koolproxyR package/openwrt-packages
